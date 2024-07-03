b37c380ee4107ffa6d0cbb7fa8d3438052f8f061
DougzCD <magonegrodcd@gmail.com>
2024-07-03 08:50:40 -0300

    aula 013072024

--- app/main.py
+++ app/main.py
@@ -9,39 +9,27 @@ def main(pagina: ft.Page):
     # Funçoes da Pagina
     
     def btn_click(e):
-        #if not txt_name.value:
-            txt_name.error_text = "Please enter your name"
-            pagina.update
-        #else:
-            name = txt_name.value
-            pagina.clean()
-            pagina.add(ft.Text(f"Hello, {name}!"))
-
-    def checkbox_changed(e):
-        output_text.value = f"You have learned how to ski {todo_check.value}."
-        pagina.update()
+        if name.value and email.value and username.value and password.value:
+            feedback.value="Cadastro bem sucedido"
+            feedback.color="green"
+            #pagina.update
+        else:
+            feedback.value="Por favor preencha todos os campos"
+            feedback.color="red"
+        pagina.update
         
-    def button_clicked(e):
-        output_text.value = f"Dropdown value is: {color_dropdown.value}"
-        pagina.update()
     
     # Elementos da Pagina
     
-    t = ft.Text(value="Ola!", color="red")
-    btn = ft.ElevatedButton("Click me!", on_click=btn_click)
-    txt_name = ft.TextField(label="Your name")
-    output_text = ft.Text()
-    todo_check = ft.Checkbox(label="ToDo: learn how to ski", value=False, on_change=checkbox_changed)         
-    submit_btn = ft.ElevatedButton(text="Submit", on_click= button_clicked)
-    color_dropdown = ft.Dropdown(
-        width=100,
-        options = [
-            ft.dropdown.Option("Red"),
-            ft.dropdown.Option("Green"),
-            ft.dropdown.Option("Blue"),
-        ],
-    )
-    pagina.add(t, btn, txt_name, todo_check, output_text, color_dropdown, submit_btn)
+    name = ft.TextField(label="name", width =200)
+    email = ft.TextField(label="E-Mail", width =200)
+    username = ft.TextField(label="username", width =200)
+    password = ft.TextField(label="password",password=True , width =200)
+    feedback = ft.Text(value="")
+    
+    btn_logar = ft.ElevatedButton("cadastrar", on_click=btn_click)
+
+    pagina.add(name, email, username, password, feedback, btn_logar)
     
     pagina.update()
 
