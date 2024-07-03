a0416e689188ee95b443d642c766bfe973fdde32
DougzCD <magonegrodcd@gmail.com>
2024-07-03 11:14:34 -0300

    seila

--- app/cadastro.py
+++ app/cadastro.py
@@ -1,38 +1,36 @@
 import flet as ft
-import time
 
-# FUNÇÃO PARA A TELA INICIAL
-def main_screen(pagina: ft.Page):
+# Define your main function to handle a page
+def main(pagina: ft.Page):
 
-    pagina.title = "TELA INICIAL"
-    pagina.clean()
-    text = ft.Text("Bem-vindo ao Wizard Showdown",
-                    style=ft.TextThemeStyle.DISPLAY_LARGE)
-    pagina.add(text)
+    pagina.title = "Pagina Teste"
+    pagina.vertical_alignment = ft.MainAxisAlignment.CENTER
     
-# FUNÇÃO PARA A TELA SPLASH
-def splash_screen(pagina: ft.Page):
-    pagina.title="SPLASH SCREEN"
-    logo = ft.Image(src="https://e7.pngegg.com/pngimages/446/995/png-clipart-pixel-dungeon-pixel-art-boss-others-miscellaneous-game.png",
-                    width=500,height=500)
-    text = ft.Text("Carregando...",
-                   style=ft.TextThemeStyle.DISPLAY_LARGE,
-                   color="blue")
-    progress_bar = ft.ProgressBar(width=200)
-    pagina.add(logo, text, progress_bar)
+    # Funçoes da Pagina
     
-    # SIMULA UM TEMPO DE CARREGAMENTO DDE X SEGUNDOS COM ATUALIZAÇÃO DO PROGRESSO
-    for i in range(100):
-        progress_bar.value = i/100
-        pagina.update()
-        time.sleep(0.03)
-        
-    main_screen(pagina)
+    def btn_click(e):
+        if name.value and email.value and username.value and password.value:
+            feedback.value="Cadastro bem sucedido"
+            feedback.color="green"
+            #pagina.update
+        else:
+            feedback.value="Por favor preencha todos os campos"
+            feedback.color="red"
+        pagina.update
+         
+    # Elementos da Pagina
+    
+    name = ft.TextField(label="name", width =200)
+    email = ft.TextField(label="E-Mail", width =200)
+    username = ft.TextField(label="username", width =200)
+    password = ft.TextField(label="password",password=True , width =200)
+    feedback = ft.Text(value="")
+    
+    btn_logar = ft.ElevatedButton("cadastrar", on_click=btn_click)
 
+    pagina.add(name, email, username, password, feedback, btn_logar)
+    
+    pagina.update()
 
-# FUNÇÃO PRINCIPAL
-def main(pagina:ft.Page):
-    
-    splash_screen(pagina)
-    
+# Run the application using ft.app, with main as the entry point
 ft.app(target=main)
